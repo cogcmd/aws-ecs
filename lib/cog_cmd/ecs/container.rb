a8e2cfbdaa1b58beb3658d7ed935e607cd645ce4
Matthew Peck <matt@operable.io>
2016-09-02 11:16:55 -0400

    ecs:container delete

--- lib/cog_cmd/ecs/container.rb
+++ lib/cog_cmd/ecs/container.rb
@@ -2,7 +2,7 @@ require_relative 'aggregate_command_with_usage'
 
 class CogCmd::Ecs::Container < CogCmd::Ecs::AggregateCommandWithUsage
 
-  SUBCOMMANDS = %w(list show define)
+  SUBCOMMANDS = %w(list show define delete)
 
   USAGE = <<~END
   Usage: ecs:container <subcommand> [options]
---
+++ lib/cog_cmd/ecs/container/delete.rb
@@ -0,0 +1,28 @@
+require_relative '../helpers'
+
+class CogCmd::Ecs::Container::Delete < Cog::SubCommand
+
+  include CogCmd::Ecs::Helpers
+
+  USAGE = <<~END
+  Usage: ecs:container delete <name>
+
+  Deletes the specified container definition
+  END
+
+  def run_command
+    unless def_name = request.args[0]
+      msg = "You must specify a definition name."
+      raise CogCmd::Ecs::ArgumentsError, msg
+    end
+
+    client = Aws::S3::Client.new()
+
+    key = "#{container_definition_root[:prefix]}#{def_name}.json"
+    client.delete_object(bucket: container_definition_root[:bucket], key: key)
+
+    { status: "deleted",
+      name: def_name }
+  end
+
+end
