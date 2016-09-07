580bcf2aeab291536434eff4b3e05237b6fa5486
Matthew Peck <matt@operable.io>
2016-09-07 14:16:19 -0400

    ecs:task deregister

--- lib/cog_cmd/ecs/task.rb
+++ lib/cog_cmd/ecs/task.rb
@@ -2,13 +2,15 @@ require 'ecs/aggregate_command_with_usage'
 
 class CogCmd::Ecs::Task < CogCmd::Ecs::AggregateCommandWithUsage
 
-  SUBCOMMANDS = %w(register list families)
+  SUBCOMMANDS = %w(register list families deregister)
 
   USAGE = <<~END
   Usage: ecs:task <subcommand> [options]
 
   Subcommands:
     register <family> <def1 | def2 | ...>    Registers a new task
+    deregister <family:revision>             Deregisters the specified task.
+                                             Note: A family and revision must be specified
     list                                     Lists registered tasks and their revisions
     families                                 Lists task definition families
 
---
+++ lib/cog_cmd/ecs/task/deregister.rb
@@ -0,0 +1,24 @@
+require 'ecs/helpers'
+
+class CogCmd::Ecs::Task::Deregister < Cog::SubCommand
+
+  include CogCmd::Ecs::Helpers
+
+  USAGE = <<~END
+  Usage: ecs:task deregister <family:revision>
+
+  Deregisters a task
+  Note: Both family and revision must be specified
+  END
+
+  def run_command
+    unless task = request.args[0]
+      raise CogCmd::Ecs::ArgumentError, "You must specify a family and revision 'family:revision'"
+    end
+
+    client = Aws::ECS::Client.new()
+
+    client.deregister_task_definition(task_definition: task).to_h
+  end
+
+end
