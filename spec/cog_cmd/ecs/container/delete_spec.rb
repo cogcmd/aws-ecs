490eb654406019d7da9f1e1c7097d1b7d334a16c
Matthew Peck <matt@operable.io>
2016-09-13 16:39:33 -0400

    container-delete spec

---
+++ spec/cog_cmd/ecs/container/delete_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe 'the container-delete command' do
+  let(:command_name) { 'container-delete' }
+
+  let(:client) do
+    client = Object.new
+    allow(Aws::S3::Client).to receive(:new).and_return(client)
+    client
+  end
+
+  it 'deletes a container definition' do
+    ENV['ECS_CONTAINER_DEF_URL'] = 's3://fakebucket/container-definitions'
+    expect(client).to receive(:delete_object)
+
+    run_command(args: ['mydef'])
+
+    expect(command).to respond_with({ status: 'deleted',
+                                      name: 'mydef' })
+  end
+
+  it 'returns an error when a definition name is not passed' do
+    expect { run_command }.to raise_error(Cog::Error)
+  end
+
+end
