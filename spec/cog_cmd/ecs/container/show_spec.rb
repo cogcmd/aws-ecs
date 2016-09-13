8b64c183fbd426f8f345ec59a69ce5110411aa8c
Matthew Peck <matt@operable.io>
2016-09-13 16:48:51 -0400

    container-show spec

---
+++ spec/cog_cmd/ecs/container/show_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe 'the container-show command' do
+  let(:command_name) { 'container-show' }
+
+  let(:client) do
+    client = Object.new
+    allow(Aws::S3::Client).to receive(:new).and_return(client)
+    client
+  end
+
+  it 'shows a container definition' do
+    ENV['ECS_CONTAINER_DEF_URL'] = 's3://fakebucket/container-definitions'
+    container_def = double(body: double(read: '{"name":"mydef","memory":256,"image":"myimage"}'))
+
+    expect(client).to receive(:get_object).and_return(container_def)
+
+    run_command(args: ['mydef'])
+
+    expect(command).to respond_with({ 'name' => 'mydef',
+                                      'image' => 'myimage',
+                                      'memory' => 256 })
+  end
+
+end
