2b442fee43d50cbbf6ef2c4f30bd283b7dc265b0
Matthew Peck <matt@operable.io>
2016-09-14 16:01:55 -0400

    ecs:service-update

---
+++ lib/cog_cmd/ecs/service/update.rb
@@ -0,0 +1,48 @@
+require 'cog_cmd/ecs/helpers'
+require 'cog_cmd/ecs/service/helpers'
+
+module CogCmd::Ecs::Service
+  class Update < Cog::Command
+
+    include CogCmd::Ecs::Helpers
+    include CogCmd::Ecs::Service::Helpers
+
+    attr_reader :service_name
+    attr_reader :cluster_name, :task_definition, :desired_count
+
+    def initialize
+      # args
+      @service_name = request.args[0]
+
+      # options
+      @cluster_name = request.options['cluster']
+      @deployment_config = request.options['deployment-config']
+      @task_definition = request.options['task-definition']
+      @desired_count = request.options['desired-count']
+    end
+
+    def run_command
+      raise(Cog::Error, "A service name must be specified.") unless service_name
+
+      response.template = 'service_show'
+      response.content = update_service
+    end
+
+    private
+
+    def update_service
+      client = Aws::ECS::Client.new()
+
+      params = {
+        service: service_name
+      }
+      params['cluster'] = cluster_name                       if cluster_name
+      params['deployment_configuration'] = deployment_config if deployment_config
+      params['task_definition'] = task_definition            if task_definition
+      params['desired_count'] = desired_count                if desired_count
+
+      client.update_service(params).service.to_h
+    end
+
+  end
+end
---
+++ spec/cog_cmd/ecs/service/update_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe 'the service-update command' do
+  let(:command_name) { 'service-update' }
+
+  let(:client) { Object.new }
+
+  let(:service) do
+    Aws::ECS::Types::UpdateServiceResponse.new
+  end
+
+  before do
+    allow(Aws::ECS::Client).to receive(:new).and_return(client)
+  end
+
+  it 'updates a service' do
+    expect(client).to receive(:update_service).and_return(service)
+
+    run_command(args: ['myservice'])
+  end
+
+  it 'returns an error when a service name is not specified' do
+    expect { run_command }.to raise_error(Cog::Error)
+  end
+
+end
